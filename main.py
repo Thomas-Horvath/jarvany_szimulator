3f72cfb2eec4ec4c430b19902e8fcc740ff42693
Thomas-Horvath <thomas.horvath0416@gmail.com>
2025-01-27 21:34:21 +0100

    infection simulator

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from random import randint   # random modulból importáljuk a randintet
 
 # * konstansok  /változó nevét nagy betűvel írjuk ezzel jelezzük, hogy konstans/
 SCREEN_SIZE = WIDTH, HEIGHT = (1280, 720)
-NUMBER_OF_PEOPLE = 10
-PERSON_SPEED = 15
+NUMBER_OF_PEOPLE = 100
+PERSON_SPEED = 30
 PERSON_COLOR_CLEAR = (200, 150, 0)
 PERSON_COLOR_INFECTED = (200, 0, 0)
 PERSON_COLOR_IMMUNE = (0, 150, 0)
 PERSON_AURA = 20
+ILLNESS_TIME = 500
+IMMUNE_TIME = 800
+INFECTION_PROBABILITY_WITH_MASK = 50
+INFECTION_PROBABILITY_WITHOUT_MASK = 2
+FIRST_CONTACT = 1
 
 
 # * inicializálás /kezdeti feltételek létrehozása/
@@ -28,6 +33,9 @@ class Person:
         self.y = y
         self.dx = randint(-1, 1)
         self.dy = randint(-1, 1)
+        self.illness = 0
+        self.immune = 0
+        self.mask = False
 
     def move(self):
         if randint(1, 30) == 1:
@@ -40,19 +48,56 @@ class Person:
         if PERSON_AURA < self.y + self.dy < HEIGHT - PERSON_AURA:
             self.y += self.dy
 
+        
+    def cure(self):
+        if self.illness > 0:
+            self.illness -= 1
+            if self.illness == 0:
+                self.immune =IMMUNE_TIME
+        if self.immune > 0:
+            self.immune -= 1
+
+
 
 def modify(people):
     for person in people:
         person.move()
+        person.cure()
+
+        for other_person in people:
+            if id(person) != id(other_person):
+                if other_person.x - 2*PERSON_AURA < person.x < other_person.x + 2*PERSON_AURA \
+                    and other_person.y - 2*PERSON_AURA < person.y < other_person.y + 2*PERSON_AURA:
+                    
+                    if other_person.illness > 0 and person.illness == 0 and person.immune == 0:
+
+                        if person.mask == True or other_person.mask == True:
+                            if randint(1, INFECTION_PROBABILITY_WITH_MASK) == 1:
+                                person.illness = ILLNESS_TIME
+                        else:
+                            if randint(1, INFECTION_PROBABILITY_WITHOUT_MASK) == 1:
+                                person.illness = ILLNESS_TIME
+
 
 
 def draw(people):
     screen.fill((20, 20, 20))
+    
 
     for person in people:
+
+        if person.illness > 0:
+            _color = PERSON_COLOR_INFECTED
+
+        elif person.immune > 0:
+            _color = PERSON_COLOR_IMMUNE
+        else:
+            _color = PERSON_COLOR_CLEAR
+
+
         pygame.draw.circle(
             screen,
-            PERSON_COLOR_CLEAR,
+            _color,
             (person.x, person.y),
             PERSON_AURA,
             0
@@ -65,6 +110,12 @@ def draw(people):
 # * emberek létrehozása
 main_people_list = [Person(randint(50, WIDTH-50), randint(50, HEIGHT-50))
                     for i in range(NUMBER_OF_PEOPLE)]
+
+
+# * első fertőzések
+
+for i in range(FIRST_CONTACT):
+    main_people_list[i].illness = ILLNESS_TIME
 
 
 # * fő programrész
